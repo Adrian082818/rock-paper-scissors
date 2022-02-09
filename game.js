f075a83eeb88c6727aab3f23a9e1cf6163c48ea0
adrian mohabir <mohabiradrian@gmail.com>
2022-02-09 11:30:06 -0500

    adding function to play one round of rock paper scissors

--- game.js
+++ game.js
@@ -1,9 +1,19 @@
-
-// let random = items[Math.floor(Math.random() * items.length)]
-
 function computerPlay() {
     let items = ["Rock", "Paper", "Scissors"]
     let random = items[Math.floor(Math.random() * items.length)]
     return random
 }
-console.log(computerPlay())
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "Rock".toLowerCase() && computerSelection === "Paper") {
+        return "You Lose! Paper beats Rock"
+    } 
+}
+
+// function caseInsensitive(playerSelection, computerSelection) {
+//     return playerSelection.localeCompare(computerSelection, undefined, {sensitivity: 'accent' }) === 0
+// }
+
+const playerSelection = "rocK".toLowerCase()
+const computerSelection = computerPlay()
+
+console.log(playRound(playerSelection, computerSelection))
