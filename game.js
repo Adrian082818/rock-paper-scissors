8b5cb39cd76d609f3eec4b02d4599c9f2fd52181
adrian mohabir <mohabiradrian@gmail.com>
2022-02-09 19:13:27 -0500

    adding prompt to playerselection

--- game.js
+++ game.js
@@ -3,20 +3,39 @@ function computerPlay() {
     let random = items[Math.floor(Math.random() * items.length)]
     return random
 }
+
+const prompt = require("prompt-sync")();
+
+const playerSelection = prompt("Choose your weapon: ")
+const computerSelection = computerPlay()
+
 function playRound(playerSelection, computerSelection) {
     let humanPlayer = playerSelection.toLowerCase()
     let t1000 = computerSelection.toLowerCase()
+
+    let humanScore = 0
+    let computerScore = 0
+
     if (humanPlayer === "Rock".toLowerCase() && t1000 === "Paper".toLowerCase()) {
-        return "You Lose! Paper beats Rock"
-    } 
+        computerScore = 1
+        console.log(computerScore)
+        return "You Lose! Paper beats Rock. Computer Wins!"
+    } else if (humanPlayer === "Scissors".toLowerCase() && t1000 === "Paper".toLowerCase()) {
+        humanScore = 1
+        console.log(humanScore)
+        return "You Win! Scissors beats Paper. Humans Win!"
+    }
+
 }
 
-// function caseInsensitive(playerSelection, computerSelection) {
-//     return playerSelection.localeCompare(computerSelection, undefined, {sensitivity: 'accent' }) === 0
-// }
+function game() {
+    // let score = 0
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound(playerSelection, computerSelection))
+    }
+}
+ console.log(game())
 
-const playerSelection = "RoCk"
-const computerSelection = computerPlay()
 // console.log(playerSelection, computerSelection)
 
-console.log(playRound(playerSelection, computerSelection))
+// console.log(playRound(playerSelection, computerSelection))
