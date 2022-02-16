d71e08d46c3792b6cf295a3908ced31bdc4d3b19
adrian mohabir <mohabiradrian@gmail.com>
2022-02-16 11:22:16 -0500

    changing game function and playround function so player is reprompt

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 function computerPlay() {
-    let items = ["Rock", "Paper", "Scissors"]
-    let random = items[Math.floor(Math.random() * 3)]
-    return random.toLowerCase()
+	let items = ["Rock", "Paper", "Scissors"]
+	let random = items[Math.floor(Math.random() * 3)]
+	return random.toLowerCase()
 }
 
 const prompt = require("prompt-sync")({sigint: true});
@@ -15,8 +15,8 @@ let computerScore = 0
 let scoreLimit = 5
 let gameOver = false 
 
-// create variables to keep track of scoring  done
-// set a limit to the score of 5   done 
+// create variables to keep track of scoring  
+// set a limit to the score of 5   
 // check for gameover to be false then continue the game 
 // once a player has reached the score limit of 5 its game over 
 // set gameover to true end game
@@ -24,50 +24,50 @@ let gameOver = false
 
 
 function playRound() {
-    let playerSelection = prompt("Choose your weapon: ").toLowerCase()
-    const computerSelection = computerPlay()
 
-    if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore += 1
-        return `You Lose! Paper beats Rock. computer score: ${computerScore}`
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        humanScore += 1
-        return `You Win! Rock beats Scissors. human score: ${humanScore}`
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore += 1
-        return `You Lose! scissors beats paper. computer score: ${computerScore}`
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        humanScore += 1
-        return `You Win! paper beats rocks. human score: ${humanScore}`
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore += 1
-        return `You Lose! rock beats scissors. computer score: ${computerScore}`
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        humanScore += 1
-        return `You Win! scissors beats paper. human score: ${humanScore}`
-    } else if (playerSelection === computerSelection) {
-        return "Draw! Both the same"
-    } 
+	let playerSelection = prompt("Choose your weapon: ").toLowerCase()
+	const computerSelection = computerPlay()
+
+	if (playerSelection === "rock" && computerSelection === "paper") {
+		computerScore += 1
+		return `You Lose! Paper beats Rock. computer score: ${computerScore}`
+	} else if (playerSelection === "rock" && computerSelection === "scissors") {
+		humanScore += 1
+		return `You Win! Rock beats Scissors. human score: ${humanScore}`
+	} else if (playerSelection === "paper" && computerSelection === "scissors") {
+		computerScore += 1
+		return `You Lose! scissors beats paper. computer score: ${computerScore}`
+	} else if (playerSelection === "paper" && computerSelection === "rock") {
+		humanScore += 1
+		return `You Win! paper beats rocks. human score: ${humanScore}`
+	} else if (playerSelection === "scissors" && computerSelection === "rock") {
+		computerScore += 1
+		return `You Lose! rock beats scissors. computer score: ${computerScore}`
+	} else if (playerSelection === "scissors" && computerSelection === "paper") {
+		humanScore += 1
+		return `You Win! scissors beats paper. human score: ${humanScore}`
+	} else if (playerSelection === computerSelection) {
+		return "Draw! Both the same"
+	} 
 }
 
 function game() {
-    console.log(playRound())
-    if (humanScore < 5 && computerScore < 5) {
-        game()
-    } else {
-        endGame()
-    }
+	console.log(playRound())
+	if (humanScore < 5 && computerScore < 5) {
+		game()
+	} else {
+		endGame()
+	}
 }
  console.log(game())
 // game()
 
  function endGame() {
-     if (humanScore === scoreLimit) {
-         console.log("winner humans")
-         gameOver = true 
-     } else if (computerScore === scoreLimit) {
-         console.log("computerrrs winss")
-         gameOver = true 
-     }
+	 if (humanScore === scoreLimit) {
+		 console.log("winner humans")
+		 gameOver = true 
+	 } else if (computerScore === scoreLimit) {
+		 console.log("computerrrs winss")
+		 gameOver = true 
+	 }
  }
-// console.log(playRound(playerSelection, computerSelection))
